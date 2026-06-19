ee14b323b69745ee72a63e407b48268613af4452
Bruno Santos <bside89@live.com>
2026-06-19 02:21:14 -0300

    feat: solve problem 125: valid palindrome

---
+++ 125-valid-palindrome/125.go
@@ -0,0 +1,33 @@
+package validpalindrome
+
+import (
+	"strings"
+	"unicode"
+)
+
+func isPalindrome(s string) bool {
+	filtered := filterString(s)
+	for i, j := 0, len(filtered)-1; i < j; i, j = i+1, j-1 {
+		if filtered[i] != filtered[j] {
+			return false
+		}
+	}
+	return true
+}
+
+func filterString(s string) string {
+	var builder strings.Builder
+	builder.Grow(len(s))
+	for _, r := range s {
+		if unicode.IsLetter(r) {
+			if unicode.IsLower(r) {
+				builder.WriteRune(r)
+			} else {
+				builder.WriteRune(r + 32)
+			}
+		} else if unicode.IsNumber(r) {
+			builder.WriteRune(r)
+		}
+	}
+	return builder.String()
+}
